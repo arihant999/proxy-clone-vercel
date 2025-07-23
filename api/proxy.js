feebbdfc5404993851e364b087dbf9676d9837d7
arihant999 <arihantofficial999@gmail.com>
2025-07-23 19:51:26 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,10 +1,15 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://studyuk.fun/goal/');
-  let html = await response.text();
+  try {
+    const response = await fetch('https://studyuk.fun/goal/');
+    let html = await response.text();
 
-  html = html.replace(/https:\/\/t\.me\/[^\s"']+/g, 'https://t.me/+rc5Psv_S2VJkMGM1');
+    // Replace all Telegram links with your custom channel link
+    html = html.replace(/https:\/\/t\.me\/[^\s"'<>]+/g, 'https://t.me/+rc5Psv_S2VJkMGM1');
 
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
-  res.status(200).end(html);
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
+    res.status(200).end(html);
+  } catch (error) {
+    res.status(500).end("Something went wrong: " + error.message);
+  }
 }
