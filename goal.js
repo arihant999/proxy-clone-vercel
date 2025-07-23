8f8b1005a8ab97bb80dee0c0188e8434c64da4d7
arihant999 <arihantofficial999@gmail.com>
2025-07-23 23:07:27 +0530

    Add files via upload

---
+++ goal.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const targetUrl = "https://studyuk.fun/goal/";
+
+  try {
+    const response = await fetch(targetUrl);
+    let html = await response.text();
+
+    html = html.replace(/https:\/\/t\.me\/[^"' ]+/g, "https://t.me/+rc5Psv_S2VJkMGM1");
+
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("Cache-Control", "no-cache");
+
+    return res.status(200).send(html);
+  } catch (err) {
+    return res.status(500).send("Proxy Failed: " + err.message);
+  }
+}
