932e3333f3c3071d7ba6e53b1a5797889ef43684
arihant999 <arihantofficial999@gmail.com>
2025-07-23 23:14:55 +0530

    Update goal.js

--- api /goal.js
+++ api /goal.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
   const targetUrl = "https://studyuk.fun/goal/";
 
@@ -5,7 +7,7 @@ export default async function handler(req, res) {
     const response = await fetch(targetUrl);
     let html = await response.text();
 
-    html = html.replace(/https:\/\/t\.me\/[^"' ]+/g, "https://t.me/+rc5Psv_S2VJkMGM1");
+    html = html.replace(/https:\\/\\/t\\.me\\/[^"' ]+/g, "https://t.me/+rc5Psv_S2VJkMGM1");
 
     res.setHeader("Content-Type", "text/html");
     res.setHeader("Cache-Control", "no-cache");
