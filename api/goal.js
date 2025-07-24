6de2847717ad12439bfe559e26d19496bd8b6901
arihant999 <arihantofficial999@gmail.com>
2025-07-24 09:19:13 +0530

    Add files via upload

--- api/goal.js
+++ api/goal.js
@@ -1,20 +1,18 @@
 
 export default async function handler(req, res) {
-  const response = await fetch("https://studyuk.fun/goal", {
+  const targetUrl = "https://studyuk.fun/goal/";
+
+  const response = await fetch(targetUrl, {
     headers: {
-      "User-Agent": "Mozilla/5.0",
+      'User-Agent': req.headers['user-agent'] || 'Mozilla/5.0',
     },
   });
 
-  let html = await response.text();
+  let text = await response.text();
 
-  // Replace StudyUK branding with your Telegram links
-  html = html
-    .replace(/https:\/\/t\.me\/[^"']+/g, "https://t.me/+rc5Psv_S2VJkMGM1")
-    .replace(/@StudyUK[^"'<]*/gi, "@MockManager")
-    .replace(/StudyUK\.fun/gi, "NAKODA BHAIRAV");
+  // Replace Telegram links
+  text = text.replace(/https:\/\/t\.me\/[\w\-\+]+/g, 'https://t.me/+rc5Psv_S2VJkMGM1');
 
-  res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate=30");
-  res.setHeader("Content-Type", "text/html");
-  res.status(200).send(html);
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(text);
 }
